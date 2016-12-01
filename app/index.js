84c6ce215481537731a3e7d526739283a496c455
Stig Otnes Kolstad <stigok@gmail.com>
2016-12-01 10:20:48 +0100

    Simplify code

--- app/index.js
+++ app/index.js
@@ -2,17 +2,17 @@ const mongoose = require('mongoose')
 
 const host = process.env.MONGO_HOST
 const db = process.env.MONGO_DB
-const cnnstr = `mongodb://${host}/${db}`
 
-
-const connection = mongoose.connect(cnnstr, (err) => {
+mongoose.connect(`mongodb://${host}/${db}`, err => {
   if (err) {
     console.error('Could not connect to database', cnnstr)
     process.exit(1)
   }
+
   console.log('Connected to mongodb!')
+
   mongoose.disconnect(() => {
     console.log('Disconnected from mongodb')
-  });
+  })
 })
 
